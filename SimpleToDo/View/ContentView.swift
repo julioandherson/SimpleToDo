a7f1366a013d198965c44a3a6d03b8bc9ce91017
Júlio Andherson <julioandherson1@gmail.com>
2025-06-20 23:56:03 -0300

    fixing color

--- SimpleToDo/View/ContentView.swift
+++ SimpleToDo/View/ContentView.swift
@@ -27,7 +27,6 @@ struct ContentView: View {
                         })
                         {
                             Image(systemName: "calendar.circle.fill")
-                                .foregroundStyle(.gray)
                                 .padding(.leading)
                         }
                         
